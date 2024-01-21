589e6e3d3c65adfe72fa069bd56a62bbc1cac206
Anith Prakash <64299704+AnithPrakash@users.noreply.github.com>
2024-01-21 15:09:14 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,74 @@
+import pickle
+import streamlit as st
+
+model=(open("D:/DATA SCIENCE/PROJECTS DONE BY ME/P2/models.sav"))
+model1=pickle.loads(open(model,'rb'))
+
+st.title("Predict your Credit Score")
+
+col1, col2, col3, col4, col5, col6=st.columns(6)
+with col1:
+    ID=st.text_input("Enter your userID")
+
+with col2:
+    Annual_Income=st.text_input("Enter your Annual income")
+
+with col3:
+    N_Bank_acc=st.text("Enter number of Bank account you have")
+
+with col4:
+    N_credit_card=st.text_input("Enter the number of credit card")
+
+with col5:
+    Interest=st.text_input("Enter your interest Rate ")
+
+with col6:
+    num_loan=st.text_input("enter your number of loan")
+
+with col1:
+    type_loan=st.text_input("enter your Type of loan ( specify how in number )")
+
+with col2:
+    Num_of_Delayed_Payment=st.text_input("Enter your Num_of_Delayed_Payment")
+
+with col3:
+    Changed_Credit_Limit=st.text_input("enter your Changed_Credit_Limit")
+
+with col4:
+    Num_Credit_Inquiries=st.text_input("Enter your Num_Credit_Inquiries")
+
+with col5:
+    credit_mix=st.text_input("Enter your credit mix")
+
+with col6:
+    Outstanding_Debt=st.text_input("Enter your Outstanding_Debt")
+
+with col1:
+    Credit_Utilization_Ratio=st.text_input("Enter your Credit_Utilization_Ratio")
+
+with col2:
+    Payment_of_Min_Amount=st.text_input("enter your Payment_of_Min_Amount")
+
+with col3:
+    Total_EMI_per_month=st.text_input("enter your Total_EMI_per_month")
+
+with col4:
+    Amount_invested_monthly=st.text_input("Enter your Amount_invested_monthly")
+
+with col5:
+    Payment_Behaviour=st.text_input("Enter your Payment_Behaviour")
+
+with col6:
+    Monthly_Balance=st.text_input("Enter your Monthly_Balance")
+
+credit_score=""
+if st.button("Result"):
+    pred=model1.predict(
+        [[ID,Annual_Income,Num_Bank_Accounts,Num_Credit_Card,Interest_Rate,Num_of_Loan,Type_of_Loan,Num_of_Delayed_Payment,Changed_Credit_Limit,Num_Credit_Inquiries,Credit_Mix,Outstanding_Debt,Credit_Utilization_Ratio,Payment_of_Min_Amount,Total_EMI_per_month,Amount_invested_monthly,Payment_Behaviour,Monthly_Balance]])
+    if pred[0]==0:
+        print("your credit score is good")
+    elif pred[1]==1:
+        print("your credit score is standard")
+    else:
+        print("your credit score is low")
+st.success(pred)
